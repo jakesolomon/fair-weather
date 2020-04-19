53117d04077a4e30edf15be89edb7c1b7fc1e816
jakesolomon <jacob_solomon@student.uml.edu>
2020-04-19 15:06:50 -0400

    Map added

---
+++ src/components/map.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+
+import './map.css';
+
+const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
+function Map() {
+  return (
+    <div className="map">
+      <ComposableMap>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
+          }
+        </Geographies>
+      </ComposableMap>
+    </div>
+  );
+}
+
+export default Map;
