4b8abdd487531607fe176da58907aad2c69d6f81
jakesolomon <jacob_solomon@student.uml.edu>
2020-04-20 09:05:23 -0400

    Changed Map, added coords
    Changed to US map. Added a JSON file of state coordinates to put into weather fetch.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Map from './components/map.js';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  // componentDidMount() {
+  //   fetch("https://community-open-weather-map.p.rapidapi.com/weather?callback=test&id=2172797&units=%2522metric%2522%20or%20%2522imperial%2522&mode=xml%252C%20html&q=London%252Cuk", {
+  //   	"method": "GET",
+  //   	"headers": {
+  //   		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+  //   		"x-rapidapi-key": "428321991cmsh2446fdc7261ff64p136d20jsn11609dc86cab"
+  //   	}
+  //   })
+  //   .then(response => {
+  //   	console.log(response);
+  //   })
+  //   .catch(err => {
+  //   	console.log(err);
+  //   });
+  // }
+
+  render() {
+    return (
+      <div className="App">
       <Map/>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/map.js
+++ src/components/map.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import { Graticule, ComposableMap, Geographies, Geography } from "react-simple-maps";
 
 import './map.css';
 
-const geoUrl =
-  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
+
+// import coordinates
+// https://www.latlong.net/category/states-236-14.html
 
 function Map() {
   return (
     <div className="map">
-      <ComposableMap>
+      <ComposableMap projection="geoAlbersUsa">
         <Geographies geography={geoUrl}>
           {({ geographies }) =>
-            geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
+            geographies.map(geo =>
+              <Geography
+                key={geo.rsmKey}
+                stroke="#DDD"
+                geography={geo}
+                fill="#AAA"
+              />)
           }
         </Geographies>
       </ComposableMap>
