a0a090b41b4172b4c7b1ac92b2c8d72985cfd83a
jakesolomon <jacob_solomon@student.uml.edu>
2021-01-17 20:43:15 -0500

    Updates

---
+++ precache-manifest.9e85dc660fe6bbda7122aa10d8e74155.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df7060e24f6852ebdf13e9703f7279a",
+    "url": "/fair-weather/index.html"
+  },
+  {
+    "revision": "37817d262d3700ac2743",
+    "url": "/fair-weather/static/css/main.cc40fdec.chunk.css"
+  },
+  {
+    "revision": "aef8d5a45c979636d8c5",
+    "url": "/fair-weather/static/js/2.efc12ed1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/fair-weather/static/js/2.efc12ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37817d262d3700ac2743",
+    "url": "/fair-weather/static/js/main.f51ecb65.chunk.js"
+  },
+  {
+    "revision": "9bf22205135eaab3c1d6",
+    "url": "/fair-weather/static/js/runtime-main.295c054d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fair-weather/static/media/logo.5d5d9eef.svg"
+  }
+]);
